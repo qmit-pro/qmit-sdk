cfeab3d6aa44a1a370a425f5e095c9c050c150ba
Dong Wook Kim <dehypnosis@gmail.com>
2020-05-27 10:35:22 +0900

    v1.0.14

--- sdk/node/dist/vault/index.spec.js
+++ sdk/node/dist/vault/index.spec.js
@@ -5,6 +5,20 @@ describe("vault module", () => {
     it("vault.installGuide should be string", () => {
         expect(index_1.vault.installGuide).toEqual(expect.any(String));
     });
+    it("vault.fetch should work with sandbox", () => {
+        expect(index_1.vault.fetch(async (get, list, s = { appEnv: "dev", fuck: true }) => {
+            return {
+                env: "test",
+                example: (await get("common/data/test")).data,
+            };
+        }, {
+            sandbox: {
+                appEnv: "test",
+            },
+        })).toStrictEqual(expect.objectContaining({
+            env: "test",
+            example: expect.anything(),
+        }));
+    });
 });
-// ...
 //# sourceMappingURL=index.spec.js.map
--- sdk/node/src/vault/index.spec.ts
+++ sdk/node/src/vault/index.spec.ts
@@ -4,6 +4,20 @@ describe("vault module", () => {
   it("vault.installGuide should be string", () => {
     expect(vault.installGuide).toEqual(expect.any(String));
   });
-});
 
-// ...
+  it("vault.fetch should work with sandbox", () => {
+    expect(vault.fetch(async (get, list, s = { appEnv: "dev", fuck: true } as any) => {
+      return {
+        env: "test",
+        example: (await get("common/data/test")).data,
+      };
+    }, {
+      sandbox: {
+        appEnv: "test",
+      },
+    })).toStrictEqual(expect.objectContaining({
+      env: "test",
+      example: expect.anything(),
+    }));
+  })
+});
